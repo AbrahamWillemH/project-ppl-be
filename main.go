c5d0715f6b0ced9c09c5f910eb43fb83be6dd834
Abe <117052559+AbrahamWillemH@users.noreply.github.com>
2025-02-28 17:56:31 +0700

    fix cors policy

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 
+	"github.com/gin-contrib/cors"
+
 	"project-ppl-be/config"
 	_ "project-ppl-be/docs"
 	"project-ppl-be/server"
@@ -15,5 +17,13 @@ func main() {
 
 	// Start the Gin server AFTER setting up the database
 	router := server.SetupRouter()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+
 	log.Fatal(router.Run(":8000"))
 }
